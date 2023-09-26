2d52b4bae4f2368592f7b2e7c068b90b651b07a6
kitydomin <143028891+kitydomin@users.noreply.github.com>
2023-09-26 19:40:21 -0300

    Delete git trabajo/tandil-tpe-2023/carrito.js

--- git trabajo/tandil-tpe-2023/carrito.js
+++
@@ -1,7 +0,0 @@
-// Script para la funcionalidad del carrito de compras
-const addToCartButton = document.querySelector('button');
-
-addToCartButton.addEventListener('click', () => {
-    // Agregar lógica para agregar el producto al carrito aquí
-    console.log('Producto agregado al carrito');
-});
